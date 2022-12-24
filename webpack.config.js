8de5809cd005025f19d6042b63159a38eb20e2c6
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2022-12-24 19:25:51 +0300

    the last one

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./script.js",
+  entry: "./script.ts",
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
